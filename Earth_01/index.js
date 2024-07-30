57a734fa2f0c842f21ae5af2cafb90dc2f15f157
x3b3ec <o.labrecque.lacasse@gmail.com>
2024-07-29 23:41:14 -0400

    +++

--- Earth_01/index.js
+++ Earth_01/index.js
@@ -30,40 +30,33 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 // Grab height and width of the window
 const w = window.innerWidth;
 const h = window.innerHeight;
-
 // Renderer to render the scene
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(w, h);
-
 // Append the renderer to the body of the document
 document.body.appendChild(renderer.domElement);
-
 //  Set up Camera -> Camera(fov, aspect ratio, near, far)
 const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 10);
-
 // Set the position of the camera, more the z value is, more the camera is far from the object
 camera.position.z = 3
-
 // Create a scene
 const scene = new THREE.Scene();
-
+// Add earthGroup to the scene -> to make the earth turn on itself with the right axis
+const earthGroup = new THREE.Group();
+earthGroup.rotation.y = -23.4 * Math.PI / 180;
+scene.add(earthGroup);
 // Add OrbitControls to the scene
 const controls = new OrbitControls(camera, renderer.domElement);
-
 //  Add damping to the controls -> controls.enableDamping = true -> to make the camera move smoothly
 controls.enableDamping = true;
-
 // Add factor to the damping -> controls.dampingFactor = 0.25 -> to make the camera move smoothly
 controls.dampingFactor = 0.25;
-
 // IcosahedronGeometry(radius, detail) -> detail is the number of times the geometry is subdivided 
 // The higher the detail, the more the geometry is subdivided -> more the geometry is smooth
-const geo = new THREE.IcosahedronGeometry(1.0,  14cd );
-
+const geo = new THREE.IcosahedronGeometry(1.0,  14);
 // Load a texture
 // https://threejs.org/docs/#api/en/textures/Texture
 const loader = new THREE.TextureLoader();
-
 // Add Material on this Geometry
 // https://threejs.org/docs/#api/en/materials/MeshBasicMaterial.aoMapIntensity
 const mat = new THREE.MeshStandardMaterial({ 
@@ -71,21 +64,16 @@ const mat = new THREE.MeshStandardMaterial({
   map: loader.load('./earthlights1k.jpg'),
   map: loader.load('./earthmap1k.jpg'),
 });
-
 // Set up Mesh -> Mesh(geometry, material)
 // Mesh is an object that takes a geometry, and applies a material to it
 const mesh = new THREE.Mesh(geo, mat); 
-
 // Add the mesh to the scene
-scene.add(mesh);
-
+earthGroup.add(mesh);
 // Need to add light to use the MeshStandardMaterial
-const skyColor = 0xffffff;
-const groundColor = 0x444444;
-const intensity = 5;
-const light = new THREE.HemisphereLight(skyColor, groundColor, intensity);
-scene.add(light);
-
+const sunColor = 0xffffff;
+const sunLight = new THREE.DirectionalLight(sunColor, 1);
+sunLight.position.set(-2, 0.5, 1.5);
+scene.add(sunLight);
 // Animation Loop -> make it turn on itself
 function animate() {
   // requestAnimationFrame(callback) -> callback is the function to be called
@@ -100,6 +88,5 @@ function animate() {
 } 
 
 animate();
-
 // Render the scene -> render(scene, camera) Render must be called every time the scene changes
 renderer.render(scene, camera);
